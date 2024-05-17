7312871c2bcf0e6020a791e0b9f38c53fd856529
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-05-17 00:41:33 -0700

    adjusted the logo with

--- src/components/GWXLogo.tsx
+++ src/components/GWXLogo.tsx
@@ -1,5 +1,5 @@
 import GWXImg from "../assets/gwx.png"
 
 export const GWXLogo = () => {
-    return <img src={GWXImg} alt="gwx logistics" width={150} height="auto" className="w-[50px] md:block" />
+    return <img src={GWXImg} alt="gwx logistics" width={150} height="auto" className="w-[50px] md:w-[150px] md:block" />
 }
